065dbf9235252225fb98ac2eaa682ce384dc7906
alfajarjaya <sansanjaya40@gmail.com>
2024-02-13 18:46:54 +0700

    c

--- kelas10.js
+++ kelas10.js
@@ -171,7 +171,7 @@ btnReset.style.backgroundColor = "red";
 function checkPassword() {
   var password = document.getElementById("password").value;
 
-  if (password === "1928") {
+  if (password === "0934") {
     document.getElementById("passwordForm").style.display = "none";
     document.querySelector(".main").style.display = "block";
   } else {
